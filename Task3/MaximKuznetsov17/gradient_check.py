2c4d811314583ba6ceffe0ddea3e647b88b6ac54
MaxKuznetsov17 <MaksKus1999@yandex.ru@>
2020-03-12 23:45:50 +0300

    Task 3.1 & 3.2 done

--- Task3/MaximKuznetsov17/gradient_check.py
+++ Task3/MaximKuznetsov17/gradient_check.py
@@ -17,24 +17,39 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
     """
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
-
+    
+    orig_x = x.copy()    
     fx, analytic_grad = f(x)
+    assert np.all(np.isclose(orig_x, x, tol))
+    
+    analytic_grad = analytic_grad.copy()
+    
+    assert analytic_grad.shape == x.shape
+    
     analytic_grad = analytic_grad.copy()
 
-    assert analytic_grad.shape == x.shape
-
+    # We will go through every dimension of x and compute numeric
+    # derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        
+        leftarg = x.copy()
+        leftarg[ix] += delta
+        rightarg = x.copy()
+        rightarg[ix] -= delta
+        fxleft, _ = f(leftarg)
+        fxright, _ = f(rightarg)
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
-
+        if isinstance(fxleft, float):
+            numeric_grad_at_ix = (((fxleft - fxright) / (2 * delta)))
+        else:
+            numeric_grad_at_ix = (((fxleft[ix[0]] - fxright[ix[0]]) / (2 * delta)))
+      
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
-                  ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+                                                               
             return False
 
         it.iternext()
